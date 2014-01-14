177b8b47970aaa9b3793072a0ceadfd2892f8649
Jennifer Cross <jcross@eastsideprep.org>
2014-01-14 14:26:06 -0800

    tweaked some JS
    still need to get Grid optomized

--- app/assets/javascripts/grid.js
+++ app/assets/javascripts/grid.js
@@ -326,7 +326,7 @@ var Grid = (function() {
             // create Preview structure:
             this.$title = $( '<h3></h3>' );
             this.$description = $( '<p></p>' );
-            this.$href = $( '<a href="#">Visit website</a>' );
+            this.$href = $( '' );
             this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
             this.$loading = $( '<div class="og-loading"></div>' );
             this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
@@ -381,7 +381,7 @@ var Grid = (function() {
 
             // preload large image and add it to the preview
             // for smaller screens we don´t display the large image (the media query will hide the fullimage wrapper)
-            if( self.$fullimage.is( ':visible' ) ) {
+            if( false && self.$fullimage.is( ':visible' ) ) {
                 this.$loading.show();
                 $( '<img/>' ).load( function() {
                     var $img = $( this );
--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -3,7 +3,7 @@ module CatalogHelper
 
   def modalities_for_course_offering(course_offering)
     #change this into a looped partial call to modalities
-    image_tag("icon.png")
+    #image_tag("icon.png")
   end
   
 end
