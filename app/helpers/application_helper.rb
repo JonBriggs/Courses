ce04df23edb055e2c5c43c792a384fd2f29844cf
Jonathan <jonathan.briggs@gmail.com>
2014-01-22 16:10:50 -0800

    added bootstrap and markdown
    sent email to matt regarding things that showed up in the excel document

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def markdown(text)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true,
+	  hard_wrap: true
+    }    
+	markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+	markdown.render(text).html_safe
+  end
+  
+    
 end
