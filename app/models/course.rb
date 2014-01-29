b9c89a10788025def68c5d3644ceed8ed8c60bb1
Jonathan <jonathan.briggs@gmail.com>
2014-01-28 21:03:48 -0800

    truncated titles, CSS tweaks and colored bars

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,12 @@ class Course < ActiveRecord::Base
   has_many :course_offerings
   belongs_to :department
   has_one :current_course_offering, class_name: CourseOffering, conditions: proc {["year_id = ?",Year.next_year.id]}
+
+  def fitted_name
+    if name.size > 41
+      name[0,38] + "..."
+    else
+      name
+    end
+  end
 end
