d0a36def6ba2b031d54936bd149da0a1be002194
Jonathan Briggs <jbriggs@eastsideprep.org>
2017-06-06 10:27:56 -0700

    fixed pwd to git sharing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,10 @@
 Courses::Application.configure do
+  config.before_configuration do
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    YAML.load(File.open(env_file)).each do |key, val|
+      ENV[key.to_s] = val
+    end if File.exists?(env_file)
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
