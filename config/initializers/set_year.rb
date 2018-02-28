a271410625fa0542b61a64bb9c6b48e43448d753
Derek Clarke <dclarke@eastsideprep.org>
2018-02-28 11:28:03 -0800

    Course info cards

--- config/initializers/set_year.rb
+++ config/initializers/set_year.rb
@@ -1,6 +1,6 @@
 #sets global year
 if Rails.env == "development"
-  Courses::Application.config.catalog_year = 18
+  Courses::Application.config.catalog_year = 19
 else
   Courses::Application.config.catalog_year = 19
 end
