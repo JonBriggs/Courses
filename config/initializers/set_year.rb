77be36264ed54d27451f4588736e4f8cdedbd785
Jonathan Briggs <jbriggs@eastsideprep.org>
2024-02-07 10:33:00 -0800

    moving to the clouds

--- config/initializers/set_year.rb
+++ config/initializers/set_year.rb
@@ -1,6 +1,6 @@
 #sets global year
 if Rails.env == "development"
-  Courses::Application.config.catalog_year = 24
+  Courses::Application.config.catalog_year = 25
 else
-  Courses::Application.config.catalog_year = 24
+  Courses::Application.config.catalog_year = 25 #this indicates the year the courses will run. I.E. if it is January 2024, we are in the 24-25 school year for the catalog which is id 25
 end
